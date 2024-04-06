a158f9478355cfac3005670f732b661b32939e4e
kyoungminLee <leegyoungmin994@gmail.com>
2024-04-07 05:06:31 +0900

    backup1

--- old_webscraper/methods.py
+++ old_webscraper/methods.py
@@ -1,4 +1,12 @@
 
 name = "lee"
 
-print(name.capitalize())#python methods.py <- 콘솔창에서 실행해주거나 우측 상단의 디코딩 버튼을 눌러야 실행이 된다. 
+# print(name.capitalize())#python methods.py <- 콘솔창에서 실행해주거나 우측 상단의 디코딩 버튼을 눌러야 실행이 된다. 
+ 
+# list, tuple, dictionary _ 오늘 배울것. 
+
+day_of_week =["Mon","Tue","Wed","Thu","Fri"]
+
+# print(day_of_week)
+# print(day_of_week.startswith("M"))//ttributeError: 'list' object has no attribute 'startswith'
+# print(name.startswith("l"))//True
