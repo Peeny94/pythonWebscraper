f27f5c2207c3c6cd0a75b45bba4fdeaaf459fcef
kyoungminLee <leegyoungmin994@gmail.com>
2024-04-23 20:17:44 +0900

    내일은 코드챌린지 강의듣기

--- ch06_class03.py
+++ ch06_class03.py
@@ -36,15 +36,14 @@ def scrape_page(url):
             "url": f"https://weworkremotely.com{url}",
         }
         all_jobs.append(job_data)
-        
-response =requests.get("https://weworkremotely.com/remote-full-time-jobs?page=1")
-
-soup = BeautifulSoup(response.content, "html.parser")
-
-buttons =  len(soup.find("div", class_="pagination").find_all("span", class_="page")) #page 양을 통해 반복문 횟수를 결정한다.
-
+def get_pages(url):    
+    response =requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    buttons =  len(soup.find("div", class_="pagination").find_all("span", class_="page")) #page 양을 통해 반복문 횟수를 결정한다.
+    return buttons
+totla_pages = get_pages("https://weworkremotely.com/remote-full-time-jobs?page=1")
 #scrap_page에서 따로 url 을 정의하지 않는 건 하기 반복문으로 전체 페이징 코드를 작성하기 때문.
-for x in range(buttons):
+for x in range(totla_pages):
     # 리스트에 특성에 따라 페이지 1부터로 임의로 숫자를 변경함.
     url = f"https://weworkremotely.com/remote-full-time-jobs?page={x+1}"
     scrape_page(url)
